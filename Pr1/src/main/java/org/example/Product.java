e1ceeb8549d303dfeba01c567b89c2477e37c13c
Dimab01112 <118934619+Dimab01112@users.noreply.github.com>
2024-09-05 14:52:15 +0300

    Add files via upload

---
+++ Pr1/src/main/java/org/example/Product.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id; // Унікальний ідентифікатор товару
+    private String name; // Назва товару
+    private double price; // Ціна товару
+    private String description; // Опис товару
+    private Category category;
+
+
+    @Override
+    public String toString() {
+        return "Товар{" +
+                "id=" + id +
+                ", назва='" + name + '\'' +
+                ", ціна=" + price +
+                ", опис='" + description + '\'' +
+                ", категорія='" + category.getName() + '\'' +
+                '}';
+    }
+
+}
